e2be20cc1e7a63030f1f7b136733559f186c51ba
Samantha Zenezi <samanthazenezi@gmail.com>
2024-01-30 14:43:58 -0300

    ajustes

--- obj/Debug/net8.0/Api.GameScrum.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.GameScrum.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api.GameScrum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d98a656e1d1a924b14e03e704ddb8c99bcbdda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api.GameScrum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api.GameScrum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
