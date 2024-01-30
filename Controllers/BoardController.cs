29d98a656e1d1a924b14e03e704ddb8c99bcbdda
Samantha Zenezi <samanthazenezi@gmail.com>
2024-01-30 14:20:50 -0300

    estrutura API

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,42 @@
+﻿using GameScrum.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameScrum.Api.Controllers
+{
+    [ApiController]
+    [Route("/tabuleiro")]
+    public class BoardController : Controller
+    {
+        private List<string> _players = new List<string>();
+
+        [HttpPost]
+        public async Task<IActionResult> InserirNome([FromBody] string nome)
+        {
+            if (_players.Count > 15)
+            {
+                return BadRequest("A sessão do jogo já está cheia.");
+            }
+            else
+            {
+                _players.Add(nome);
+            }
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Teste()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+    }
+}
---
+++ Service/PlayersService.cs
@@ -0,0 +1,14 @@
+﻿using GameScrum.Api.Models;
+
+namespace GameScrum.Api.Service
+{
+    public class PlayersService
+    {
+        public List<PlayerModel> listPlayers;
+
+        public PlayersService()
+        {
+            listPlayers = new List<PlayerModel>();
+        }
+    }
+}
